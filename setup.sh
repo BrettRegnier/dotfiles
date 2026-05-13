0ee7dcf61efd561701905f480553097fe3b79263
Brett Regnier <bretternestregnier@gmail.com>
2026-05-12 21:20:47 -0600

    create setup file

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash 
+
+DOTFILES="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+
+#### nvim ####
+rm -rf ~/.config/nvim 
+ln -s "$DOTFILES/nvim" ~/.config/nvim
+
+#### ghostty ####
+rm -rf ~/.config/ghostty
+ln -s "$DOTFILES/ghostty" ~/.config/ghostty
+
+#### alacritty ####
+rm -rf ~/.config/alacritty
+ln -s "$DOTFILES/alacritty" ~/.config/alacritty
+
+#### tmux ####
+rm -rf ~/.config/tmux
+ln -s "$DOTFILES/tmux" ~/.config/tmux
+
+#### hypr ####
+rm -rf ~/.config/hypr
+ln -s "$DOTFILES/hypr" ~/.config/hypr
