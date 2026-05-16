f61ea73889ba28427d840a4959e4331849abbf1d
Brett Regnier <bretternestregnier@gmail.com>
2026-05-15 21:55:03 -0600

    Do a better job of readme

--- README.md
+++ README.md
@@ -1,5 +1,35 @@
-╔═══════════════════════════════════════════════════════╗
-║                                                       ║
-║   My dotfiles are not the best, but they are mine. 😎 ║
-║                                                       ║
-╚═══════════════════════════════════════════════════════╝
+# ✦ dotfiles
+
+```
+  ╔═══════════════════════════════════════════════════════╗
+  ║                                                       ║
+  ║   My dotfiles are not the best, but they are mine. 😎 ║
+  ║                                                       ║
+  ╚═══════════════════════════════════════════════════════╝
+```
+
+---
+
+## What's in here
+
+> A collection of configs, scripts, and mostly aesthestically broken designs 
+
+```
+## Installation
+
+```bash
+git clone https://github.com/brettregnier/dotfiles ~/.dotfiles
+cd ~/.dotfiles
+./setup.sh   
+```
+
+> ⚠️ **Don't blindly run this.** Read the scripts.
+
+---
+
+## A note on "best practices"
+
+There are no best practices in this dotfile, just fun!
+
+---
+
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,62 +1,61 @@
 -- ~/.config/nvim/lua/plugins/treesitter.lua
 
 return {
-    "nvim-treesitter/nvim-treesitter",
-    tag = "v0.10.0",
-    build = ":TSUpdate",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-        "nvim-treesitter/nvim-treesitter-textobjects",
-    },
-    config = function()
-        require("nvim-treesitter").setup({
-            ensure_installed = {
-                "lua",
-                "vim",
-                "vimdoc",
-                "python",
-                "javascript",
-                "typescript",
-                "json",
-                "html",
-                "css",
-                "bash",
-                "markdown",
-                "markdown_inline",
-                "c",
-                "cpp",
-                "rust",
-            },
-            ignore_install = {},
-            sync_install = true,
-            modules = {},
-            auto_install = true,
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
-            indent = { enable = true },
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = "<C-space>",
-                    node_incremental = "<C-space>",
-                    scope_incremental = false,
-                    node_decremental = "<bs>",
-                },
-            },
-            textobjects = {
-                select = {
-                    enable = true,
-                    lookahead = true,
-                    keymaps = {
-                        ["af"] = "@function.outer",
-                        ["if"] = "@function.inner",
-                        ["ac"] = "@class.outer",
-                        ["ic"] = "@class.inner",
-                    },
-                },
-            },
-        })
-    end,
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-textobjects",
+	},
+	config = function()
+		require("nvim-treesitter").setup({
+			ensure_installed = {
+				"lua",
+				"vim",
+				"vimdoc",
+				"python",
+				"javascript",
+				"typescript",
+				"json",
+				"html",
+				"css",
+				"bash",
+				"markdown",
+				"markdown_inline",
+				"c",
+				"cpp",
+				"rust",
+			},
+			ignore_install = {},
+			sync_install = true,
+			modules = {},
+			auto_install = true,
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+			indent = { enable = true },
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<C-space>",
+					node_incremental = "<C-space>",
+					scope_incremental = false,
+					node_decremental = "<bs>",
+				},
+			},
+			textobjects = {
+				select = {
+					enable = true,
+					lookahead = true,
+					keymaps = {
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						["ac"] = "@class.outer",
+						["ic"] = "@class.inner",
+					},
+				},
+			},
+		})
+	end,
 }
